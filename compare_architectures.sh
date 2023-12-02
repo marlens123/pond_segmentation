2c4af08810bf0a372210e3b24e81e47c50a76d26
marlens123 <marlena@itdex.de>
2023-12-02 11:02:27 +0100

    small change

--- compare_architectures.sh
+++ compare_architectures.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Investigating if attention is worth it..."
+#echo "Investigating if attention is worth it..."
 
-python fine_tune.py --pref test_att_unet --path_to_config config/att_unet.json
-python fine_tune.py --pref test_unet --path_to_config config/best_unet.json
+#python fine_tune.py --pref test_att_unet --path_to_config config/att_unet.json
+#python fine_tune.py --pref test_unet --path_to_config config/best_unet.json
 
-echo "Model selection done. Logs are stored in 'metrics/scores/fine_tune/'."
+#echo "Model selection done. Logs are stored in 'metrics/scores/fine_tune/'."
+
+echo "Investigating if pspnet is worth it..."
+
+#python fine_tune.py --pref test_psp_net --path_to_config config/psp_net.json
+python predict.py --pref unet --data IRdata_ATWAICE_processed_220718_142920.nc --weights_path weights/unet/best_modeltest_unet.h5 --convert_to_grayscale --skip_preprocessing
+
+echo "Done."
