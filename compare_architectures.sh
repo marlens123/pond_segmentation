dc0ac7ec44339efe9bc9afc0bf2d9a402cef7b7f
marlens123 <80780236+marlens123@users.noreply.github.com>
2023-12-02 10:58:33 +0100

    Delete compare_architectures.sh

--- compare_architectures.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Investigating if attention is worth it..."
-
-python fine_tune.py --pref test_att_unet --path_to_config config/att_unet.json
-python fine_tune.py --pref test_unet --path_to_config config/best_unet.json
-
-echo "Model selection done. Logs are stored in 'metrics/scores/fine_tune/'."
